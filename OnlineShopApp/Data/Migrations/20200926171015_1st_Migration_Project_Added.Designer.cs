bef42e6ac71e78356f58d228ccceffff12a230fc
GH Palash <golamhabibpalash@gmail.com>
2020-09-26 23:18:59 +0600

    1st_Migration_Added_

--- OnlineShopApp/Data/Migrations/20200926171015_1st_Migration_Project_Added.Designer.cs
+++ OnlineShopApp/Data/Migrations/20200926171015_1st_Migration_Project_Added.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineShopApp.Data;
 
 namespace OnlineShopApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200926171015_1st_Migration_Project_Added")]
+    partial class _1st_Migration_Project_Added
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OnlineShopApp/Data/Migrations/20200926171015_1st_Migration_Project_Added.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShopApp.Data.Migrations
+{
+    public partial class _1st_Migration_Project_Added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
