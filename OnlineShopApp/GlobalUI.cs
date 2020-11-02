aab0316c7133f420aacca39d790f598e080f6c9d
GH Palash <golamhabibpalash@gmail.com>
2020-11-02 07:17:39 +0600

    Theme Installed

---
+++ OnlineShopApp/GlobalUI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopApp
+{
+    public class GlobalUI
+    {
+        public static string PageTitle { get; set; }
+
+    }
+}
